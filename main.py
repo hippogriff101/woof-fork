3a33dfa911908bcb1e04603b8e32ea732e366e88
hippogriff101 <freddieayershon@gmail.com>
2026-01-03 12:01:03 +0000

    Dog Page

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/dogs')
+def new_page():
+    return render_template('dogs.html')
+
 if __name__ == '__main__':
     app.run()
